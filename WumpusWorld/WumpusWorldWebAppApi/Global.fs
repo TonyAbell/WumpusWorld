faa9d4a75edee5dbef7ddbd1888a1c1065c32457
Tony Abell <tony@tonyabell.com>
2013-08-18 17:44:39 -0700

    added boot strap, removed old jquery files

--- WumpusWorld/WumpusWorldWebAppApi/Global.fs
+++ WumpusWorld/WumpusWorldWebAppApi/Global.fs
@@ -10,31 +10,39 @@ open System.Web.Optimization
 
 type BundleConfig() =
     static member RegisterBundles (bundles:BundleCollection) =
-        bundles.Add(ScriptBundle("~/bundles/jquery").Include([|"~/Scripts/jquery-1.*"|]))
+        bundles.Add(ScriptBundle("~/bundles/jquery").Include([|"~/Scripts/jquery-2*"|]))
 
-        bundles.Add(ScriptBundle("~/bundles/jqueryui").Include([|"~/Scripts/jquery-ui*"|]))
+        //bundles.Add(ScriptBundle("~/bundles/jqueryui").Include([|"~/Scripts/jquery-ui*"|]))
 
-        bundles.Add(ScriptBundle("~/bundles/jqueryval").Include(
-                                     [|"~/Scripts/jquery.unobtrusive*";
-                                     "~/Scripts/jquery.validate*"|]))
+
+
+//        bundles.Add(ScriptBundle("~/bundles/jqueryval").Include(
+//                                     [|"~/Scripts/jquery.unobtrusive*";
+//                                     "~/Scripts/jquery.validate*"|]))
             
         bundles.Add(ScriptBundle("~/bundles/modernizr").Include([|"~/Scripts/modernizr-*"|]))
 
-        bundles.Add(StyleBundle("~/Content/css").Include([|"~/Content/*.css"|]))
+        bundles.Add(ScriptBundle("~/bundles/bootstrap").Include([|"~/Scripts/bootstrap*"|]))
 
-        bundles.Add(StyleBundle("~/Content/themes/base/css").Include(
-                                    "~/Content/themes/base/jquery.ui.core.css",
-                                    "~/Content/themes/base/jquery.ui.resizable.css",
-                                    "~/Content/themes/base/jquery.ui.selectable.css",
-                                    "~/Content/themes/base/jquery.ui.accordion.css",
-                                    "~/Content/themes/base/jquery.ui.autocomplete.css",
-                                    "~/Content/themes/base/jquery.ui.button.css",
-                                    "~/Content/themes/base/jquery.ui.dialog.css",
-                                    "~/Content/themes/base/jquery.ui.slider.css",
-                                    "~/Content/themes/base/jquery.ui.tabs.css",
-                                    "~/Content/themes/base/jquery.ui.datepicker.css",
-                                    "~/Content/themes/base/jquery.ui.progressbar.css",
-                                    "~/Content/themes/base/jquery.ui.theme.css"))
+        //bundles.Add(StyleBundle("~/Content/css").Include([|"~/Content/*.css"|]))
+
+//        bundles.Add(StyleBundle("~/Content/themes/base/css").Include(
+//                                    "~/Content/themes/base/jquery.ui.core.css",
+//                                    "~/Content/themes/base/jquery.ui.resizable.css",
+//                                    "~/Content/themes/base/jquery.ui.selectable.css",
+//                                    "~/Content/themes/base/jquery.ui.accordion.css",
+//                                    "~/Content/themes/base/jquery.ui.autocomplete.css",
+//                                    "~/Content/themes/base/jquery.ui.button.css",
+//                                    "~/Content/themes/base/jquery.ui.dialog.css",
+//                                    "~/Content/themes/base/jquery.ui.slider.css",
+//                                    "~/Content/themes/base/jquery.ui.tabs.css",
+//                                    "~/Content/themes/base/jquery.ui.datepicker.css",
+//                                    "~/Content/themes/base/jquery.ui.progressbar.css",
+//                                    "~/Content/themes/base/jquery.ui.theme.css"))
+
+        bundles.Add(StyleBundle("~/Content/css").Include("~/Content/bootstrap.css",
+                                                         //"~/Content/site.css",
+                                                               "~/Content/bootstrap-responsive.css"))
 
 type Route = { controller : string; action : string; id : UrlParameter }
 type ApiRoute = { id : RouteParameter }
